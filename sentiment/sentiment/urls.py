51aeffa9bdc5f2b97af9827ef28f50d3b95337e0
Siddhali Kathar <SiddhaliK@users.noreply.huggingface.co>
2026-03-18 04:19:00 +0000

    Update sentiment/sentiment/urls.py

--- sentiment/sentiment/urls.py
+++ sentiment/sentiment/urls.py
@@ -15,20 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from analysis import views
+from django.urls import path, include
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
-    path('college_info', views.college_info, name='college_info'),
-    path('Aboutus', views.Aboutus, name='Aboutus'),
-    #path('review', views.review, name='review'),
-    path('review', views.sentiment_analysis_view, name='sentiment-review'),
-    path('get_college_reviews', views.get_college_reviews, name='get_college_reviews'),
-   #path('api/get_college_reviews/', views.get_college_reviews, name='get_college_reviews')
-
+    path('', include('analysis.urls')),
 ]
-
 
 
 
